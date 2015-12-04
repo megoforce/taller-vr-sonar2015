5586a4c2b61b54153d98e9a07ee4830a8bd9c8f4
Julio Quiroz <chulini@gmail.com>
2015-12-04 19:20:46 -0300

    desplazamiento de player

---
+++ taller-sonar-2015/Assets/Scripts/OculusMode.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OculusMode : MonoBehaviour {
+	void Start () {
+		UnityEngine.VR.VRSettings.loadedDevice = UnityEngine.VR.VRDeviceType.Oculus;
+	}
+}
+
